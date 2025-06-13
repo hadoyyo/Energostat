7a9fd240acbf13c9abeb0efcdbe8dd9a579fbf2e
Maciej Kaminski <maciej.kam03@gmail.com>
2025-06-13 16:08:24 +0200

    Fix year input bug

--- client/src/components/YearRangeInput.jsx
+++ client/src/components/YearRangeInput.jsx
@@ -1,30 +1,32 @@
-export default function YearRangeInput({ 
-  startYear, 
-  endYear, 
-  onStartYearChange, 
-  onEndYearChange 
+export default function YearRangeInput({
+  startYear,
+  endYear,
+  onStartYearChange,
+  onEndYearChange
 }) {
   return (
     <div className="input-range">
       <div className="input-group">
         <label htmlFor="start-year">From:</label>
-        <input 
-          type="number" 
-          id="start-year" 
-          min="1960" 
-          max="2023" 
+        <input
+          type="number"
+          id="start-year"
+          min="2000"
+          max="2023"
+          onKeyDown={(e) => e.preventDefault()}
           value={startYear}
           onChange={(e) => onStartYearChange(parseInt(e.target.value))}
         />
       </div>
-      
+
       <div className="input-group">
         <label htmlFor="end-year">To:</label>
-        <input 
-          type="number" 
-          id="end-year" 
-          min="1960" 
-          max="2023" 
+        <input
+          type="number"
+          id="end-year"
+          min="2000"
+          max="2023"
+          onKeyDown={(e) => e.preventDefault()}
           value={endYear}
           onChange={(e) => onEndYearChange(parseInt(e.target.value))}
         />
